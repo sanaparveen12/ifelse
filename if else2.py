6ff499cb7dca6de57b90e4a9058e6d88c0d9a293
Pragati Rahul <pragatir18@navgurukul.org>
2020-06-02 12:53:51 +0530

    first commit

---
+++ if else2.py
@@ -0,0 +1,10 @@
+ day=input("enter a day")
+if day==("monday") or day==("Monday"):
+    print("today is monday")
+elif day==("tuesday"):
+       print("today is tuesday")
+elif day==("wednesday"):
+       print("today is wednesday")
+else:
+   print("today is holiday enjoy hurrah!")
+
---
+++ sana if else/code.py
@@ -0,0 +1,27 @@
+# num= input("enter the num")
+# number = int(num)
+# if (number %2!=0):
+#   print("this number is multiple of 3")
+# else:
+#     print("odd  hai number")
+    
+# a=1000
+# if a>= 4000:
+#     print("bada ya barabar hai")
+# else: 
+#     print("chotha hai")
+
+# a=1000
+# if a==1000:
+#    print("barabar hai")
+# else:
+#     print("barabar nahi hai")
+
+# a=100
+# b=33
+# if a-b==35:
+#     print("33 kai barabar hai")
+# else:
+#     print("barabar nahi hai")
+
+
